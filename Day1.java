2f968275d819fd878b45912fe1ddb8f6ac871ce6
Chris Scheers <chris.scheers@siemens.com>
2022-12-07 00:19:40 +0100

    For day1

--- Day1.java
+++ Day1.java
@@ -1,18 +1,30 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class Day1 {
 
     static void readFile(File file) throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
+        int sum = 0;
+        Queue<Integer> queue = new PriorityQueue<Integer>();
         while(scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            int value = Integer.valueOf(line);
-            System.out.println("Calories:", value);
+            if (line.length() == 0) {
+                queue.offer(sum);
+                if (queue.size() > 3) {
+                    queue.poll();
+                }
+                sum = 0;
+            } else {
+                sum += Integer.valueOf(line);
+            }
         }
-        System.out.println("Number of increasing measurements: " + increasing);
-        System.out.println("Number of increasing sliding window measurements: " + increasingWindow);
+        // Assume last line is an empty line
+        int topThree = queue.poll() + queue.poll() + queue.poll();
+        System.out.println("Top 3: " + topThree);
     }
 
     public static void go() throws FileNotFoundException {
---
+++ main.java
@@ -0,0 +1,32 @@
+import java.io.FileNotFoundException;
+
+public class main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Day1.go();
+//        Day2.go();
+//        Day3.go();
+//        Day4.go();
+//        Day5.go();
+//        Day6.go();
+//        Day7.go();
+//        Day8.go();
+//        Day9.go();
+//        Day10.go();
+//        Day11.go();
+//        Day12.go();
+//        Day13.go();
+//        Day14.go();
+//        Day15.go();
+//        Day16.go();
+//        Day17.go();
+//        Day18.go();
+//        Day19.go();
+//        Day20.go();
+//        Day21.go();
+//        Day22.go();
+//        Day23.go();
+//        Day24.go();
+//        Day25.go();
+    }
+}
